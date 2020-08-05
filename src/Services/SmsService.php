b50cfb325413a81b4b88dc31b55cb42c9845ca8a
chichi <kurapica@gmail.com>
2020-08-05 12:35:57 +0800

    [簡訊] 調整三竹

--- src/Services/SmsService.php
+++ src/Services/SmsService.php
@@ -11,14 +11,14 @@ class SmsService {
 	{
 		$username =  config('labspace-auth-api.sms.mitake.username');
 		$password =config('labspace-auth-api.sms.mitake.password');
-		$msg = mb_convert_encoding($msg,'big5','utf-8');
-        $sms_url = config('labspace-auth-api.sms.mitake.url').'?username='.$username.'&password='.$password.'&dstaddr='.$phone.'&DestName='.env('WEBSITE_NAME').'&dlvtime=10&vldtime=660&smbody='.$msg;
+		//$msg = mb_convert_encoding($msg,'big5','utf-8');
+        $sms_url = config('labspace-auth-api.sms.mitake.url').'?CharsetURL=UTF-8&username='.$username.'&password='.$password.'&dstaddr='.$phone.'&DestName='.env('WEBSITE_NAME').'&dlvtime=10&vldtime=660&smbody='.$msg;
       	
         $result_sms = file_get_contents($sms_url);
         $colume = explode("\r\n", $result_sms);
         $status_code = explode("=",$colume[1]);
 
-        $result_sms = iconv("big5","UTF-8",$result_sms);
+        //$result_sms = iconv("big5","UTF-8",$result_sms);
         switch ($status_code[1]) {
         	case '0':
         		return  ['status' =>true ,'detail' => $result_sms];
